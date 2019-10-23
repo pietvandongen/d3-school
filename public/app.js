aca07721f5cba0dd8da9036400a6660c11557c17
Piet van Dongen <piet.vandongen@luminis.eu>
2019-10-23 22:44:39 +0200

    Added files and data

---
+++ public/app.js
@@ -0,0 +1,28 @@
+async function run() {
+    // Get data
+    let data = await d3.csv("/data/temperatures.csv");
+
+    // Create arrays for dates and temperatures
+
+    // Extract statistics
+
+    // Show statistics
+
+    // Create graph
+
+    // Create x-scale
+
+    // Create y-scale
+
+    // Create line function
+
+    // Create canvas
+
+    // Draw x-axis
+
+    // Draw y-axis
+
+    // Draw line
+}
+
+run();
